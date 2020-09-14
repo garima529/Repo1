a500753e6c2a98825149be23c883c2b15a814a14
Garima Chawla <GARIMAC@amdocs.com>
2020-09-14 15:05:54 +0530

    added

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+//package Java_Practice.Lab1_HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a=10;
+		System.out.println(a);
+		System.out.println("Hello World");
+
+	}
+	
+
+}
